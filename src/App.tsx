ff2c7a111effe2b7b9531fe570f024ababed57b8
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-12-30 19:24:53 +0200

    basic: init project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="w-6 h-16 transition-transform transform hover:scale-110"
+            alt="React logo"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
