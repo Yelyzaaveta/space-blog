c5fcae3991294b8c8b1991d3978441fa975c29ce
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2025-01-06 23:46:18 +0200

    fix: fix planet sizing

--- src/section/PlanetsSection.tsx
+++ src/section/PlanetsSection.tsx
@@ -4,34 +4,34 @@ import Planet3 from "../assets/images/planets/planet3.png";
 
 const PlanetsSection = () => {
   return (
-    <section className="w-full flex justify-center items-center py-12 bg-dark">
-      <div className="flex flex-wrap justify-center gap-8">
+    <section className="w-full flex justify-center items-center bg-dark sm:h-[500px] md:h-full lg:h-[400px]">
+      <div className="flex flex-wrap justify-center gap-[100px]">
         <div
-          className="w-[200px] h-[200px] bg-center bg-cover rounded-full flex items-center justify-center text-white text-xl font-bold animate-smoothBounce"
+          className="w-[200px] h-[200px] xl:w-[320px] xl:h-[320px] transition-[width,height] duration-1000 bg-center bg-cover rounded-full flex items-center justify-center text-dark text-[28px] font-bold animate-smoothBounce"
           style={{
             backgroundImage: `url(${Planet1})`,
             animationDelay: "0s",
           }}
         >
-          Планета 1
+          28 stars
         </div>
         <div
-          className="w-[200px] h-[200px] bg-center bg-cover rounded-full flex items-center justify-center text-white text-xl font-bold animate-smoothBounce"
+          className="w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] xl:w-[400px] xl:h-[400px] transition-[width,height] duration-1000 bg-center bg-cover rounded-full flex items-center justify-center text-white text-[30px] font-bold animate-smoothBounce"
           style={{
             backgroundImage: `url(${Planet2})`,
             animationDelay: "1s",
           }}
         >
-          Планета 2
+          2000 planets
         </div>
         <div
-          className="w-[200px] h-[200px] bg-center bg-cover rounded-full flex items-center justify-center text-white text-xl font-bold animate-smoothBounce"
+          className="w-[220px] h-[220px] xl:w-[280px] xl:h-[280px] transition-[width,height] duration-1000 bg-center bg-cover rounded-full flex items-center justify-center text-dark text-[28px] font-bold animate-smoothBounce"
           style={{
             backgroundImage: `url(${Planet3})`,
             animationDelay: "2s",
           }}
         >
-          Планета 3
+          2 years
         </div>
       </div>
     </section>
