4cdcde722d6f0fe8ce88c409c6e185f6d0ec922e
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2025-01-07 19:22:39 +0200

    chore: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  base: 'space-blog/',
+  server: {  
     host: '0.0.0.0', 
   },
    base: '/space-blog/',
