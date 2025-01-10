66928d651ef152252b700cc58bd3d6d85da08f6b
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2025-01-10 17:13:25 +0200

    fix: animation of sattelight and add background

--- src/section/ContactSection.tsx
+++ src/section/ContactSection.tsx
@@ -1,42 +1,47 @@
+import Background from "../assets/videos/sky-background.mp4";
 import Sattelite from "../assets/images/sattelite-dark.png";
 
 const ContactSection = () => {
   return (
-    <section className="w-full mt-[100px] h-[320px] bg-dark relative overflow-hidden flex items-center justify-center">
-      <div
-        className="absolute right-0 w-full h-full animate-blackSlide z-10"
-        style={{
-          background:
-            "linear-gradient(to right, rgba(0, 0, 2, 1) 90%, rgba(0, 0, 0, 0) 100%)",
-        }}
-      ></div>
+    <section className="w-full mt-[100px] h-[320px] lg:h-[460px] bg-dark relative overflow-hidden flex items-center justify-center">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={Background} type="video/mp4" />
+        Video Error
+      </video>
+      <div className="absolute top-0 left-0 w-full h-[100px] bg-gradient-to-b from-dark via-dark/75 via-dark/50 to-transparent"></div>
+      <div className="absolute bottom-0 left-0 w-full h-[100px] bg-gradient-to-t from-dark via-dark/75 via-dark/50 to-transparent"></div>
 
-      <div className="absolute right-0 transform animate-satelliteFly z-20">
+      <div className="absolute transform animate-satelliteFly z-20">
         <img
           src={Sattelite}
           alt="Sattelite"
-          className="w-[450px] h-auto opacity-80"
+          className="w-[450px] lg:w-[550px] h-auto opacity-90"
         />
       </div>
 
       <div className="relative text-center opacity-100">
-        <h2 className="text-3xl font-bold mb-4 text-white">
+        <h2 className="text-3xl font-bold mb-4 text-light">
           Reach for the Stars
         </h2>
         <form className="w-[300px] mx-auto space-y-4">
           <input
             type="text"
             placeholder="Your Name"
-            className="w-full p-3 rounded-md bg-light text-dark placeholder-gray-400 focus:outline-none"
+            className="w-full p-3 rounded-md bg-light text-dark placeholder-white focus:outline-none"
           />
           <input
             type="email"
             placeholder="Your Email"
-            className="w-full p-3 rounded-md bg-light text-dark placeholder-gray-400 focus:outline-none"
+            className="w-full p-3 rounded-md bg-light text-dark placeholder-white focus:outline-none"
           />
           <button
             type="submit"
-            className="w-full p-3 bg-primary text-white rounded-md font-bold hover:bg-primary-dark transition duration-300"
+            className="px-[30px] p-3 bg-light text-white rounded-md font-bold hover:text-dark transition duration-300"
           >
             Send Message
           </button>
@@ -47,28 +52,21 @@ const ContactSection = () => {
         {`
           @keyframes satelliteFly {
             0% {
-              transform: translateX(100%);
+              transform: translate(200%, 10%);
+            }
+            33% {
+              transform: translate(100%, 0%);
+            }
+            66% {
+              transform: translate(0%, 10%);
             }
             100% {
-              transform: translateX(-200%);
+              transform: translate(-200%, 0%);
             }
           }
 
           .animate-satelliteFly {
-            animation: satelliteFly 5s linear forwards;
-          }
-
-          @keyframes blackSlide {
-            0% {
-              transform: translateX(10%);
-            }
-            100% {
-              transform: translateX(-100%);
-            }
-          }
-
-          .animate-blackSlide {
-            animation: blackSlide 6s linear forwards;
+            animation: satelliteFly 15s linear infinite;
           }
         `}
       </style>
